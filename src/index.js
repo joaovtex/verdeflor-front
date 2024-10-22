6c3a62ea10cc9d328a6c0f45685f54815799072b
joao <joaovictorfarias1610@gmail.com>
2024-10-22 13:57:11 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import LangingPage from './pages/landingpage';
-import Login from './pages/login'
 import Nav from './rotas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
