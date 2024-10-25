7c7120b455952004aca69c0257e2c3dbf4e9f23e
joao <joaovictorfarias1610@gmail.com>
2024-10-24 23:33:27 -0300

    criando pagina de testes

---
+++ src/pages/Teste/index.jsx
@@ -0,0 +1,9 @@
+export default function Teste() {
+    return (
+
+        <div>
+            <p>deu!</p>
+        </div>
+
+    );
+}
--- src/rotas.jsx
+++ src/rotas.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LangingPage from "./pages/landingpage";
 import Login from "./pages/login";
+import Teste from "./pages/Teste";
 
 export default function Nav() {
     return (
@@ -10,6 +11,7 @@ export default function Nav() {
             <Routes>
                 <Route path="/" element={<LangingPage/>} />
                 <Route path="/login" element={<Login/>} />
+                <Route path="/teste" element={<Teste/>}/>
             </Routes>
         </BrowserRouter>
 
