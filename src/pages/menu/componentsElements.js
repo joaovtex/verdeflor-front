5b162f221a73c20a92decbb49f93ae554dd58c32
joao <joaovictorfarias1610@gmail.com>
2024-10-25 22:44:13 -0300

    front da tela de menu 100% completado

---
+++ src/pages/menu/componentsElements.js
@@ -0,0 +1,29 @@
+const botoesElementos = [
+    {
+        titulo: "Novo Colaborador",
+        texto: "Cadastre novos colaboradores e mantenha sua equipe sempre pronta para o trabalho.",
+        image: "/assets/images/iconeNovoColaborador.png"
+    },
+    {
+        titulo: "Dados Colaboradores",
+        texto: "Consulte rapidamente os dados dos colaboradores para facilitar a gestão e o planejamento.",
+        image: "/assets/images/iconeDadosColaboradores.png"
+    },
+    {
+        titulo: "Novos Serviços",
+        texto: "Adicione novos serviços e mantenha o gerenciamento das atividades a serem prestadas sempre em dia.",
+        image: "/assets/images/iconeNovosServicos.png"
+    },
+    {
+        titulo: "Informações dos Serviços",
+        texto: "Mantenha o controle dos serviços, garantindo uma organização eficiente das tarefas.",
+        image: "/assets/images/iconeInformacoesServicos.png"
+    },
+    {
+        titulo: "Gerenciamento de Serviços",
+        texto: "Visualize o ID dos serviços e o nome dos colaboradores responsável pela execução.",
+        image: "/assets/images/iconeGerenciamentoServicos.png"
+    }
+]
+
+export default botoesElementos
--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -2,19 +2,42 @@ import './index.scss';
 import HeaderMenus from '../../components/headerMenus';
 import FooterMenus from '../../components/footerMenus';
 import BotoesMenu from '../../components/botoesMenu';
+import botoesElementos from './componentsElements.js';
 
 export default function Menu() {
+    
     return (
-
         <div className='pagina-menu'>
-            
-            <HeaderMenus/>
+
+            <HeaderMenus />
 
             <div className='corpo'>
                 <h2>Seus Gerenciamentos</h2>
-                 <BotoesMenu titulo="Novo Colaborador" texto="Cadastre novos colaboradores e 
-                 mantenha sua equipe sempre pronta para o trabalho." image="/assets/images/iconeNovoColaborador.png"/>
+
+                    <div className='botoes'>
+                        {botoesElementos.slice(0, 3).map((item, index) => (
+                            <BotoesMenu
+                                key={index}
+                                titulo={item.titulo}
+                                texto={item.texto}
+                                image={item.image}
+                            />
+                        ))}
+                    </div>
+                    <div className='botoes'>
+                        {botoesElementos.slice(3, 5).map((item, index) => (
+                            <BotoesMenu
+                                key={index + 3}
+                                titulo={item.titulo}
+                                texto={item.texto}
+                                image={item.image}
+                            />
+                        ))}
+                    </div>
+
             </div>
+
+            <FooterMenus/>
 
         </div>
 
