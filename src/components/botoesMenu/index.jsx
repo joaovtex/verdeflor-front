1340465db82d4079c6cb2e0de38f3d8bdd763ecb
joao <joaovictorfarias1610@gmail.com>
2024-10-27 18:48:10 -0300

    Link nos botões da tela de menu

--- src/components/botoesMenu/index.jsx
+++ src/components/botoesMenu/index.jsx
@@ -1,22 +1,20 @@
 import './index.scss';
+import { Link } from 'react-router-dom';
 
 export default function BotoesMenu(props) {
 
     return (
-
-        <a href={props.link} className='component-botoesmenu'>
+        <Link to={props.link} className='component-botoesmenu'>
             <div className='textos'>
                 <h3>{props.titulo}</h3>
                 <p>{props.texto}</p>
             </div>
 
             <div className='icones'>
-                <img src="/assets/images/iconeMais.png" alt="" id='plus'/>
-                <img src={props.image} alt="" id='icone'/>
+                <img src="/assets/images/iconeMais.png" alt="" id='plus' />
+                <img src={props.image} alt="" id='icone' />
             </div>
-
-        </a>
-
+        </Link>
     );
 
 }
--- src/rotas.jsx
+++ src/rotas.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import RotasProtegidas from "./rotasprotegidas.jsx";
 import LangingPage from "./pages/landingpage";
 import Login from "./pages/login";
 import Teste from "./pages/Teste";
 import Menu from "./pages/menu/";
 import NovoColaborador from "./pages/novoColaborador";
-import RotasProtegidas from "./rotasprotegidas.jsx";
+
 
 export default function Nav() {
     return (
@@ -15,8 +15,8 @@ export default function Nav() {
                 <Route path="/" element={<LangingPage/>} />
                 <Route path="/login" element={<Login/>} />
                 <Route path="/teste" element={<RotasProtegidas><Teste/></RotasProtegidas>}/>
-                <Route path="/novoColaborador"  element={<RotasProtegidas><NovoColaborador/></RotasProtegidas>}/>
                 <Route path="/menu" element={<RotasProtegidas><Menu/></RotasProtegidas>} />
+                <Route path="/novoColaborador"  element={<RotasProtegidas><NovoColaborador/></RotasProtegidas>}/>
             </Routes>
         </BrowserRouter>
 
