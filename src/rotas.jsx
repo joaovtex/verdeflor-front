32df4305d5e8cc00f01b32d630206a6ae1f9ef0b
Raissa Casilla <raissacasilla@gmail.com>
2024-10-28 21:20:27 -0300

    subindo link da página "novo serviço"

--- src/rotas.jsx
+++ src/rotas.jsx
@@ -5,6 +5,9 @@ import Login from "./pages/login";
 import Teste from "./pages/Teste";
 import Menu from "./pages/menu/";
 import NovoColaborador from "./pages/novoColaborador";
+//import EditarColaborador from "./pages/editarColaborador";
+//import ListarColaboradores from "./pages/listarColaboradores";
+import NovoServico from "./pages/novoServico";
 
 
 export default function Nav() {
@@ -16,7 +19,10 @@ export default function Nav() {
                 <Route path="/login" element={<Login/>} />
                 <Route path="/teste" element={<RotasProtegidas><Teste/></RotasProtegidas>}/>
                 <Route path="/menu" element={<RotasProtegidas><Menu/></RotasProtegidas>} />
-                <Route path="/novoColaborador"  element={<RotasProtegidas><NovoColaborador/></RotasProtegidas>}/>
+                <Route path="/novoColaborador" element={<RotasProtegidas><NovoColaborador/></RotasProtegidas>}/>
+                {/* <Route path="/editarColaborador" element={<RotasProtegidas><EditarColaborador/></RotasProtegidas>}/>
+                <Route path="/colaboradores" element={<RotasProtegidas><ListarColaboradores/></RotasProtegidas>}/>*/}
+                <Route path="/novoServico" element={<RotasProtegidas><NovoServico/></RotasProtegidas>}/>
             </Routes>
         </BrowserRouter>
 
