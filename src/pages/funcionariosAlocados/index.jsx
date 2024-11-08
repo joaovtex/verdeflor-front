fc8ead6942692ad9b54e2a55f5aa5e9539523ef7
joao <joaovictorfarias1610@gmail.com>
2024-11-07 23:01:41 -0300

    bruto do gráfico feito, falta estilizar e arrumar o css da pagina

--- src/pages/funcionariosAlocados/index.jsx
+++ src/pages/funcionariosAlocados/index.jsx
@@ -6,28 +6,53 @@ import HeaderMenus from '../../components/headerMenus'
 import FooterMenus from '../../components/footerMenus'
 import { useNavigate } from 'react-router-dom'
 import { API_URL } from '../../api/constants'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 export default function FuncionariosAlocados() {
     const navigate = useNavigate()
     const [lista, setLista] = useState([])
+    const [totalOrcamentoPorMes, setTotalOrcamentoPorMes] = useState({});
+    const [val, setVal] = useState()
 
     useEffect(() => {
         buscar()
     }, [])
 
+    const dados = [
+        { mes: 'Jan', vendas: totalOrcamentoPorMes.mes_1 },
+        { mes: 'Fev', vendas: totalOrcamentoPorMes.mes_2 },
+        { mes: 'Mar', vendas: totalOrcamentoPorMes.mes_3 },
+        { mes: 'Abr', vendas: totalOrcamentoPorMes.mes_4 },
+        { mes: 'Mai', vendas: totalOrcamentoPorMes.mes_5 },
+        { mes: 'Jun', vendas: totalOrcamentoPorMes.mes_6 },
+        { mes: 'Jul', vendas: totalOrcamentoPorMes.mes_7 },
+        { mes: 'Ago', vendas: totalOrcamentoPorMes.mes_8 },
+        { mes: 'Set', vendas: totalOrcamentoPorMes.mes_9 },
+        { mes: 'Out', vendas: totalOrcamentoPorMes.mes_10 },
+        { mes: 'Nov', vendas: totalOrcamentoPorMes.mes_11 },
+        { mes: 'Dez', vendas: totalOrcamentoPorMes.mes_12 }
+    ]
+
     async function buscar() {
         try {
             let token = localStorage.getItem('TOKEN')
 
-            let resp = await axios.get(`${API_URL}/listar/funcionarios-alocados`,
+            let resp = await axios.get(`${API_URL}/listar/faturamento-anual`,
                 {
                     headers: { 'x-access-token': token }
                 }
             )
 
-            let listaOrdenada = resp.data.sort((a, b) => b.ID - a.ID)
+            setLista(resp.data)
 
-            setLista(listaOrdenada)
+            let totalOrcamentoPorMes = {};
+
+            resp.data.forEach(item => {
+                totalOrcamentoPorMes[`mes_${item.mes}`] = item.total_orcamento;
+            });
+
+            setTotalOrcamentoPorMes(totalOrcamentoPorMes);
+
         } catch (error) {
             alert('Erro ao buscar serviço: ' + error.response.data.erro)
         }
@@ -40,6 +65,28 @@ export default function FuncionariosAlocados() {
             <HeaderMenus />
 
             <div className='corpo'>
+
+                <div className='voltarTitulo'>
+                    <img
+                        src="/assets/images/botao-voltar.png"
+                        onClick={() => navigate(-1)}
+                    />
+
+                    <h2>IDs de Serviços e Funcionários Alocados</h2>
+                </div>
+
+                <ResponsiveContainer width="80%" height={300}>
+                    <BarChart data={dados}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="mes" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="vendas" fill="#8884d8" />
+                    </BarChart>
+                </ResponsiveContainer>
+
+                {/*
                 <div className='voltarTitulo'>
                     <img
                         src="/assets/images/botao-voltar.png"
@@ -68,6 +115,7 @@ export default function FuncionariosAlocados() {
                         )}
                     </tbody>
                 </table>
+                */}
             </div>
 
             <FooterMenus />
